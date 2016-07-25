d37fd37bec45a26b61059a8bc4c4c89124ed4447
weathare <fuzuki.close@gmail.com>
2016-07-25 14:10:25 +0900

    Packages, variables, and functions.

---
+++ packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+)
+
+func main() {
+  fmt.Println("My favorit number:", rand.Intn(10))
+}
---
+++ short-variable-declarations.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  var i, j int = 1, 2
+  c, python, java := true, false, "no!"
+
+  fmt.Println(i, j, c, python, java)
+}
